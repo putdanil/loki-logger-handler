75fafe5323ffc42ed1d7d12b3403f7dbce728ffc
Daniil Putilov <75547456+putdanil@users.noreply.github.com>
2024-11-16 12:11:51 +0400

    Update streams.py

--- loki_logger_handler/streams.py
+++ loki_logger_handler/streams.py
@@ -4,8 +4,15 @@ import json
 class _LokiRequestEncoder(json.JSONEncoder):
     def default(self, obj):
         print("Data being serialized:", obj)  # Отладка
-        if isinstance(obj, Streams):
-            return {"streams": [stream.__dict__ for stream in obj.streams]}
+        return {
+                "streams": [
+                    {
+                        key: list(value) if isinstance(value, set) else value
+                        for key, value in stream.__dict__.items()
+                    }
+                    for stream in obj.streams
+                ]
+            }
         return json.JSONEncoder.default(self, obj)
 
 
